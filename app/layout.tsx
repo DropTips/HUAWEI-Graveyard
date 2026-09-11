9bc96a16f93e56c3ce5f81f7141b5860e0d6b369
Huawei Graveyard <huawei-graveyard@users.noreply.github.com>
2026-09-11 20:59:50 +0800

    feat: add contact email + update site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import '@/globals.css';
 export const metadata: Metadata = {
   title: '华为坟场 | 缅怀被华为抹杀的产品',
   description:
-    '华为坟场是一个开源静态站点，用于纪念被华为抹杀或终止的产品与服务。缅怀那些曾经深爱却已消失的华为产品。',
+    '华为坟场是一个开源静态站点，用于纪念被华为抹杀或终止的产品与服务。缅怀那些曾经深爱（或者并非）却已消失的华为产品。',
   keywords: ['华为坟场', '被华为抹杀', '华为', '荣耀', '鸿蒙', '抹杀产品'],
   icons: ['./assets/headstone.svg'],
   alternates: { canonical: '/' },
